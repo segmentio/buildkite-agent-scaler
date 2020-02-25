debd331be1c6f757113446a07693fac32dfaf859
Evan Culver <evan.culver@segment.com>
2020-02-24 17:15:40 -0800

    Fix AWS creds

--- .buildkite/steps/upload-to-s3.sh
+++ .buildkite/steps/upload-to-s3.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -eu
 
+# shellcheck disable=SC1090
+source "$SEGMENT_LIB_PATH/aws.bash"
+
 export AWS_DEFAULT_REGION=us-west-2
 # export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-$SANDBOX_AWS_ACCESS_KEY_ID}
 # export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-$SANDBOX_AWS_SECRET_ACCESS_KEY}
@@ -15,8 +18,12 @@ else
   BUCKET_PATH="${BUCKET_PATH}/builds/${BUILDKITE_BUILD_NUMBER}"
 fi
 
+function do-upload() {
+  echo "--- :s3: Uploading lambda to ${BASE_BUCKET}/${BUCKET_PATH}/ in ${AWS_DEFAULT_REGION}"
+  aws s3 cp handler.zip "s3://${BASE_BUCKET}/${BUCKET_PATH}/handler.zip"
+}
+
 echo "~~~ :buildkite: Downloading artifacts"
 buildkite-agent artifact download handler.zip .
 
-echo "--- :s3: Uploading lambda to ${BASE_BUCKET}/${BUCKET_PATH}/ in ${AWS_DEFAULT_REGION}"
-aws s3 cp handler.zip "s3://${BASE_BUCKET}/${BUCKET_PATH}/handler.zip"
+run-with-role "default" do-upload
